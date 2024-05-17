cd83691da7bc7036e82863aba4ffcb70c817fb41
Yanzaum <yandavid80@gmail.com>
2024-05-17 12:16:30 -0300

    refactor: Update Providers component to use queryClient as a constant

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -4,9 +4,9 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-export function Providers({ children }: { children: React.ReactNode }) {
-    const queryClient = new QueryClient()
+export const queryClient = new QueryClient()
 
+export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <ChakraProvider>
             <QueryClientProvider client={queryClient}>
