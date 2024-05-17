39fa2de35d2d9156af7c38454b1d9f2d981493d2
Yanzaum <yandavid80@gmail.com>
2024-05-17 10:02:53 -0300

    feat: Update EmployeesPage layout and add button create

--- frontend/src/app/employees/page.tsx
+++ frontend/src/app/employees/page.tsx
@@ -1,12 +1,13 @@
 'use client'
-import { Button } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 import EmployeesTable from "./components/EmployeesTable";
 import { Plus } from '@phosphor-icons/react';
 
 export default function EmployeesPage() {
     return (
         <main className="container mx-auto space-y-4 min-h-screen py-10">
-            <div className="flex justify-end items-center">
+            <div className="flex justify-between items-center">
+                <Text fontSize='2xl' fontWeight='bold'>Colaboradores</Text>
                 <Button leftIcon={<Plus />} colorScheme='teal' variant='solid'>
                     Adicionar
                 </Button>
