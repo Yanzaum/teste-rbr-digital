cc5aebd02033297d12628d4095b4e2d73330bb67
Yanzaum <yandavid80@gmail.com>
2024-05-17 11:39:48 -0300

    refactor: update API service base URL to include "/api"

--- frontend/src/services/api/apiService.ts
+++ frontend/src/services/api/apiService.ts
@@ -2,5 +2,5 @@ import { getServerURL } from "@/utils/getServerURL";
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: getServerURL(),
+    baseURL: getServerURL().concat("/api"),
 });
