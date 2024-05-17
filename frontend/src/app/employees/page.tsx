62cbb126da6112da6ecfe9d10ac3e089647b72fe
Yanzaum <yandavid80@gmail.com>
2024-05-17 11:40:47 -0300

    feat: add createEmployee to page

--- frontend/src/app/employees/page.tsx
+++ frontend/src/app/employees/page.tsx
@@ -1,18 +1,31 @@
 'use client'
-import { Button, Text } from "@chakra-ui/react";
+import { Button, Text, useDisclosure } from "@chakra-ui/react";
 import EmployeesTable from "./components/EmployeesTable";
 import { Plus } from '@phosphor-icons/react';
+import CreateEmployee from "./components/forms/CreateEmployee";
 
 export default function EmployeesPage() {
+    const { isOpen, onClose, onToggle } = useDisclosure({
+        defaultIsOpen: false,
+        id: 'create-employee',
+    })
+
     return (
         <main className="container mx-auto space-y-4 min-h-screen py-10">
             <div className="flex justify-between items-center">
                 <Text fontSize='2xl' fontWeight='bold'>Colaboradores</Text>
-                <Button leftIcon={<Plus />} colorScheme='teal' variant='solid'>
+                <Button
+                    type="button"
+                    leftIcon={<Plus />}
+                    colorScheme='teal'
+                    variant='solid'
+                    onClick={onToggle}
+                >
                     Adicionar
                 </Button>
             </div>
             <EmployeesTable />
+            <CreateEmployee isOpen={isOpen} onClose={onClose} />
         </main>
     )
 }
